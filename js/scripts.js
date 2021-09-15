382859673242378f2bc4eef4dcc7c4ba757abf0e
Derick Maitland <dmaitland@grupoargus.co.cr>
2021-09-15 01:22:30 -0600

    se crea navbar y se hace cambio dinamico de color

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,274 @@
 const sidebar = document.getElementById("sidebar");
 const dark = "rgba(255,255,255,0.7)";
 const light = "rgba(0,0,0,0.6)";
+const logo = document.getElementById("logo");
+const menu = document.getElementById("menu");
+
+const mlfblue = () => {
+  logo.style.background = "#3f6ad8";
+  logo.style.color = dark;
+  menu.style.background = "#3f6ad8";
+  menu.style.color = dark;
+};
+
+const mlfgrey = () => {
+  logo.style.background = "#6c757d";
+  logo.style.color = dark;
+  menu.style.background = "#6c757d";
+  menu.style.color = dark;
+};
+const mlfgreen = () => {
+  logo.style.background = "#3ac47d";
+  logo.style.color = dark;
+  menu.style.background = "#3ac47d";
+  menu.style.color = dark;
+};
+const mlflblue = () => {
+  logo.style.background = "#16aaff";
+  logo.style.color = dark;
+  menu.style.background = "#16aaff";
+  menu.style.color = dark;
+};
+const mlfyellow = () => {
+  logo.style.background = "#f7b924";
+  logo.style.color = dark;
+  menu.style.background = "#f7b924";
+  menu.style.color = dark;
+};
+const mlfred = () => {
+  logo.style.background = "#d92550";
+  logo.style.color = dark;
+  menu.style.background = "#d92550";
+  menu.style.color = dark;
+};
+const mlfwhite = () => {
+  logo.style.background = "#eee";
+  logo.style.color = light;
+  menu.style.background = "#eee";
+  menu.style.color = light;
+};
+const mlfblack = () => {
+  logo.style.background = "#343a40";
+  logo.style.color = dark;
+  menu.style.background = "#343a40";
+  menu.style.color = dark;
+};
+const mlflblack = () => {
+  logo.style.background = "#444054";
+  logo.style.color = dark;
+  menu.style.background = "#444054";
+  menu.style.color = dark;
+};
+const mlfpurple = () => {
+  logo.style.background = "#794c8a";
+  logo.style.color = dark;
+  menu.style.background = "#794c8a";
+  menu.style.color = dark;
+};
+const mlfgdarkligth = () => {
+  logo.style.backgroundImage =
+    "linear-gradient(60deg, #29323c 0%, #485563 100%)";
+  logo.style.color = dark;
+  menu.style.backgroundImage =
+    "linear-gradient(60deg, #29323c 0%, #485563 100%)";
+  menu.style.color = dark;
+};
+const mlfgdarkblue = () => {
+  logo.style.backgroundImage =
+    "linear-gradient(-20deg, #2b5876 0%, #4e4376 100%)";
+  logo.style.color = dark;
+  menu.style.backgroundImage =
+    "linear-gradient(-20deg, #2b5876 0%, #4e4376 100%)";
+  menu.style.color = dark;
+};
+const mlfgblue = () => {
+  logo.style.backgroundImage =
+    "linear-gradient(to top, #1e3c72 0%,#1e3c72 1%,#2a5298 100%)";
+  logo.style.color = dark;
+  menu.style.backgroundImage =
+    "linear-gradient(to top, #1e3c72 0%,#1e3c72 1%,#2a5298 100%)";
+  menu.style.color = dark;
+};
+const mlfgdark = () => {
+  logo.style.backgroundImage =
+    "linear-gradient(to bottom, #323232 0%, #3f3f3f 40%, #1c1c1c 150%), linear-gradient(to top, rgba(255, 255, 255, 0.4) 0%,rgba(0, 0, 0, 0.25) 200%)";
+  logo.style.color = dark;
+  menu.style.backgroundImage =
+    "linear-gradient(to bottom, #323232 0%, #3f3f3f 40%, #1c1c1c 150%), linear-gradient(to top, rgba(255, 255, 255, 0.4) 0%,rgba(0, 0, 0, 0.25) 200%)";
+  menu.style.color = dark;
+};
+const mlfghalfblackblue = () => {
+  logo.style.backgroundImage =
+    "linear-gradient(to right, #0f2027, #203a43, #2c5364)";
+  logo.style.color = dark;
+  menu.style.backgroundImage =
+    "linear-gradient(to right, #0f2027, #203a43, #2c5364)";
+  menu.style.color = dark;
+};
+const mlfgbluemarine = () => {
+  logo.style.backgroundImage = "linear-gradient(to right, #141e30, #243b55)";
+  logo.style.color = dark;
+  menu.style.backgroundImage = "linear-gradient(to right, #141e30, #243b55)";
+  menu.style.color = dark;
+};
+const mlfgpink = () => {
+  logo.style.backgroundImage =
+    "linear-gradient(45deg,#ff9a9e 0%,#fad0c4 99%,#fad0c4 100%)";
+  logo.style.color = light;
+  menu.style.backgroundImage =
+    "linear-gradient(45deg,#ff9a9e 0%,#fad0c4 99%,#fad0c4 100%)";
+  menu.style.color = light;
+};
+const mlfgpurple = () => {
+  logo.style.backgroundImage =
+    "linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%)";
+  logo.style.color = light;
+  menu.style.backgroundImage =
+    "linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%)";
+  menu.style.color = light;
+};
+const mlfgorange = () => {
+  logo.style.backgroundImage =
+    "linear-gradient(120deg, #f6d365 0%, #fda085 100%)";
+  logo.style.color = light;
+  menu.style.backgroundImage =
+    "linear-gradient(120deg, #f6d365 0%, #fda085 100%)";
+  menu.style.color = light;
+};
+const mlfglightblue = () => {
+  logo.style.backgroundImage =
+    "linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%)";
+  logo.style.color = light;
+  menu.style.backgroundImage =
+    "linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%)";
+  menu.style.color = light;
+};
+const mlfgpurplepink = () => {
+  logo.style.backgroundImage =
+    "linear-gradient(120deg, #a6c0fe 0%, #f68084 100%)";
+  logo.style.color = light;
+  menu.style.backgroundImage =
+    "linear-gradient(120deg, #a6c0fe 0%, #f68084 100%)";
+  menu.style.color = light;
+};
+const mlfggrey = () => {
+  logo.style.backgroundImage =
+    "linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)";
+  logo.style.color = light;
+  menu.style.backgroundImage =
+    "linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)";
+  menu.style.color = light;
+};
+const mlfgpinkpurple = () => {
+  logo.style.backgroundImage =
+    "linear-gradient(120deg, #fccb90 0%, #d57eeb 100%)";
+  logo.style.color = light;
+  menu.style.backgroundImage =
+    "linear-gradient(120deg, #fccb90 0%, #d57eeb 100%)";
+  menu.style.color = light;
+};
+const mlfgaqua = () => {
+  logo.style.backgroundImage =
+    "linear-gradient(to right, #4facfe 0%, #00f2fe 100%)";
+  logo.style.color = dark;
+  menu.style.backgroundImage =
+    "linear-gradient(to right, #4facfe 0%, #00f2fe 100%)";
+  menu.style.color = dark;
+};
+const mlfgbluesand = () => {
+  logo.style.backgroundImage =
+    "linear-gradient(120deg, #e0c3fc 0%, #8ec5fc 100%)";
+  logo.style.color = light;
+  menu.style.backgroundImage =
+    "linear-gradient(120deg, #e0c3fc 0%, #8ec5fc 100%)";
+  menu.style.color = light;
+};
+const mlfgpinkred = () => {
+  logo.style.backgroundImage =
+    "linear-gradient(120deg, #f093fb 0%, #f5576c 100%)";
+  logo.style.color = dark;
+  menu.style.backgroundImage =
+    "linear-gradient(120deg, #f093fb 0%, #f5576c 100%)";
+  menu.style.color = dark;
+};
+const mlfgceleste = () => {
+  logo.style.backgroundImage =
+    "radial-gradient(circle 248px at center,#16d9e3 0%,#30c7ec 47%,#46aef7 100%)";
+  logo.style.color = dark;
+  menu.style.backgroundImage =
+    "radial-gradient(circle 248px at center,#16d9e3 0%,#30c7ec 47%,#46aef7 100%)";
+  menu.style.color = dark;
+};
+const mlfglila = () => {
+  logo.style.backgroundImage =
+    "linear-gradient(135deg, #667eea 0%, #764ba2 100%)";
+  logo.style.color = dark;
+  menu.style.backgroundImage =
+    "linear-gradient(135deg, #667eea 0%, #764ba2 100%)";
+  menu.style.color = dark;
+};
+const mlfgverdeagua = () => {
+  logo.style.backgroundImage =
+    "linear-gradient(120deg, #89f7fe 0%, #66a6ff 100%)";
+  logo.style.color = light;
+  menu.style.backgroundImage =
+    "linear-gradient(120deg, #89f7fe 0%, #66a6ff 100%)";
+  menu.style.color = light;
+};
+const mlfgmusgo = () => {
+  logo.style.backgroundImage =
+    "linear-gradient(180deg, #2af598 0%, #009efd 100%)";
+  logo.style.color = dark;
+  menu.style.backgroundImage =
+    "linear-gradient(180deg, #2af598 0%, #009efd 100%)";
+  menu.style.color = dark;
+};
+const mlfgfucsia = () => {
+  logo.style.backgroundImage =
+    "linear-gradient(to top, #c471f5 0%, #fa71cd 100%)";
+  logo.style.color = dark;
+  menu.style.backgroundImage =
+    "linear-gradient(to top, #c471f5 0%, #fa71cd 100%)";
+  menu.style.color = dark;
+};
+const mlfgnaranja = () => {
+  logo.style.backgroundImage =
+    "linear-gradient(to right,#f78ca0 0%,#f9748f 19%,#fd868c 60%,#fe9a8b 100%)";
+  logo.style.color = dark;
+  menu.style.backgroundImage =
+    "linear-gradient(to right,#f78ca0 0%,#f9748f 19%,#fd868c 60%,#fe9a8b 100%)";
+  menu.style.color = dark;
+};
+const mlfgverde = () => {
+  logo.style.backgroundImage =
+    "linear-gradient(to top, #0ba360 0%, #3cba92 100%)";
+  logo.style.color = dark;
+  menu.style.backgroundImage =
+    "linear-gradient(to top, #0ba360 0%, #3cba92 100%)";
+  menu.style.color = dark;
+};
+const mlfgdurazno = () => {
+  logo.style.backgroundImage =
+    "linear-gradient(to top, #ff0844 0%, #ffb199 100%)";
+  logo.style.color = dark;
+  menu.style.backgroundImage =
+    "linear-gradient(to top, #ff0844 0%, #ffb199 100%)";
+  menu.style.color = dark;
+};
+const mlfggreydark = () => {
+  logo.style.backgroundImage =
+    "linear-gradient(to right, #434343 0%, black 100%)";
+  logo.style.color = dark;
+  menu.style.backgroundImage =
+    "linear-gradient(to right, #434343 0%, black 100%)";
+  menu.style.color = dark;
+};
+const mlfgverdedif = () => {
+  logo.style.backgroundImage = "linear-gradient(to bottom, #00b09b, #96c93d)";
+  logo.style.color = dark;
+  menu.style.backgroundImage = "linear-gradient(to bottom, #00b09b, #96c93d)";
+  menu.style.color = dark;
+};
 
 const fblue = () => {
   sidebar.style.background = "#3f6ad8";
